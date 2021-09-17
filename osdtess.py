5d5fcbc4c3e01dc967d7f30ceb6b1ac16ac56e06
SuchithH <49915548+SuchithH@users.noreply.github.com>
2021-09-17 12:11:38 +0530

    cleanup code and file structure

--- osdtess.py
+++ osdtess.py
@@ -5,32 +5,28 @@ import pytesseract
 from textblob import TextBlob
 
 def clear_image(path):
-    img = cv2.imread(path,0)
-    # img = image.copy()
-    # alpha =2.7
-    # beta = -160.0
-    # denoised = alpha * img + beta
-    # denoised = np.clip(denoised, 0, 255).astype(np.uint8)
-    # img = cv2.cvtColor(denoised, cv2.COLOR_BGR2GRAY)
-    # h = img.shape[0]
-    # w = img.shape[1]
-    # for y in range(0, h):
-    #     for x in range(0, w):
-    #         if img[y, x] >= 220:
-    #             img[y, x] = 255
-    #         else:
-    #             img[y, x] = 0
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # cv2.imwrite('output.jpg', img)
-    # return(Image.fromarray(img))
-    img = cv2.medianBlur(img,5)
-    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+    image = cv2.imread(path)
+    img = image.copy()
+    alpha =2.7
+    beta = -160.0
+    denoised = alpha * img + beta
+    denoised = np.clip(denoised, 0, 255).astype(np.uint8)
+    img = cv2.cvtColor(denoised, cv2.COLOR_BGR2GRAY)
+    h = img.shape[0]
+    w = img.shape[1]
+    for y in range(0, h):
+        for x in range(0, w):
+            if img[y, x] >= 220:
+                img[y, x] = 255
+            else:
+                img[y, x] = 0
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cv2.imwrite('output.jpg', img)
     return(Image.fromarray(img))
-tesseract_fp = r'..\tesseract.exe'
+
+tesseract_fp = r'tesseract-ocr-win64\tesseract.exe'
+im_path = r'testimg\test1.jpg'
 
 pytesseract.pytesseract.tesseract_cmd = tesseract_fp
-im_path = 'test3.jpg'
 print(pytesseract.image_to_string(clear_image(im_path)))
 print(TextBlob(pytesseract.image_to_string(clear_image(im_path))).correct())
