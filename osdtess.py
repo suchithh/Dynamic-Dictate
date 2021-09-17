666014d471dbd615ed96b363385d154f48d8e5d1
SuchithH <49915548+SuchithH@users.noreply.github.com>
2021-09-17 19:40:01 +0530

    change thresholding method

--- osdtess.py
+++ osdtess.py
@@ -4,29 +4,20 @@ import cv2
 import pytesseract
 from textblob import TextBlob
 
-def clear_image(path):
-    image = cv2.imread(path)
+def img_thresholding(path):
+    image = cv2.imread(path,0)
     img = image.copy()
-    alpha =2.7
-    beta = -160.0
-    denoised = alpha * img + beta
-    denoised = np.clip(denoised, 0, 255).astype(np.uint8)
-    img = cv2.cvtColor(denoised, cv2.COLOR_BGR2GRAY)
-    h = img.shape[0]
-    w = img.shape[1]
-    for y in range(0, h):
-        for x in range(0, w):
-            if img[y, x] >= 220:
-                img[y, x] = 255
-            else:
-                img[y, x] = 0
+    ret1,img = cv2.threshold(img,140,255,cv2.THRESH_BINARY)
+    img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     cv2.imwrite('output.jpg', img)
     return(Image.fromarray(img))
 
-tesseract_fp = r'tesseract-ocr-win64\tesseract.exe'
-im_path = r'testimg\test1.jpg'
+def detect_text(img_path):
+    tesseract_path = r'tesseract-ocr-win64\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path
+    #print(pytesseract.image_to_string(img_thresholding(img_path)))
+    return (TextBlob(pytesseract.image_to_string(img_thresholding(img_path))).correct()[:-2])
 
-pytesseract.pytesseract.tesseract_cmd = tesseract_fp
-print(pytesseract.image_to_string(clear_image(im_path)))
-print(TextBlob(pytesseract.image_to_string(clear_image(im_path))).correct())
+if __name__=="__main__":
+    print(detect_text(r'testimg\test1.jpg'))
